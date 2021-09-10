3e845e59c4e2328a94fd5aa08eab882b75d21944
Wael <wael.t.ivie@gmail.com>
2021-09-09 18:30:55 -0700

    Update app.py

--- cardano-relay-app/app.py
+++ cardano-relay-app/app.py
@@ -17,7 +17,7 @@ import numpy as np
 import descartes
 import geopandas as gpd
 
-
+#import relay data
 file = open('relay_info.json')
 data = json.load(file)
 
@@ -30,8 +30,8 @@ colors = {
     'text': '#7FDBFF'
 }
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
+# Make the dataframe from the json file
+
 df1 = pd.DataFrame()
 for i in range(len(data)):
     df1 = df1.append(pd.DataFrame(data[i]['relays']), sort=True)
@@ -39,39 +39,33 @@ for i in range(len(data)):
 df1.dropna(inplace=True)
 df1.sort_values(by=['countryCode'], inplace=True)
 
-df1.set_index('country', inplace=True)
-df_us = df1[df1.countryCode == 'US']
+#Create new df2 and then us only df2 to create the map
+df2 = df1.copy()
+df2.set_index('country', inplace=True)
+df_us = df2[df2.countryCode == 'US']
 df_us.sort_values(by=['region'], inplace=True)
 
-df_us_region = df_us['regionName'].value_counts().to_frame()
+df_us_region = df_us['region'].value_counts().to_frame()
 df_us_region['State'] = df_us_region.index
-df_us_region.rename(columns = {'regionName':'Relay Count'}, inplace = True)
+df_us_region.rename(columns = {'region':'Relay Count'}, inplace = True)
 
 
-fig = px.bar(df_us_region, x='State', y="Relay Count", color="Relay Count", barmode="group")
+# fig = px.choropleth(locations=df_us_region['State'], locationmode="USA-states", color=df_us_region['Relay Count'], scope="usa", labels={'color':'Relay Count'})
+# Plotly Express
+fig = px.choropleth(
+    data_frame=df_us_region,
+    locationmode='USA-states',
+    locations='State',
+    scope="usa",
+    color='Relay Count',
+    hover_data=['State', 'Relay Count'],
+    color_continuous_scale=px.colors.sequential.Plasma,
+    )
 
-fig.update_layout(
-    plot_bgcolor=colors['background'],
-    paper_bgcolor=colors['background'],
-    font_color=colors['text']
-)
 
-app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
-    html.H1(
-        children='Relay Information',
-        style={
-            'textAlign': 'center',
-            'color': colors['text']
-        }
-    ),
-
-    html.Div(children='Dash: A web application framework for Python.', style={
-        'textAlign': 'center',
-        'color': colors['text']
-    }),
-
+app.layout = html.Div([
     dcc.Graph(
-        id='example-graph-2',
+        id='USA Relay Distribution',
         figure=fig
     )
 ])
